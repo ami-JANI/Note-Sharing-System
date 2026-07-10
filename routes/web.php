3e1d83ec79f471a2abe00b541af3df69d82e6d97
sazzathrafee <rafee2207019@stud.kuet.ac.bd>
2026-07-10 14:27:33 +0600

    feat: add simulated credit purchase endpoint - NOTE-82/83

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CreditController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\PreviousQuestionController;
@@ -27,6 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/credits/purchase', [CreditController::class, 'purchase'])->name('credits.purchase');
 });
 
 Route::get('/users/{user}', [PublicProfileController::class, 'show'])->name('users.show');
