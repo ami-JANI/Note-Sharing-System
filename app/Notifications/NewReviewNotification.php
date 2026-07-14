f1908c5526762e589f04888915a9ed4ec16a46a6
sazzathrafee <rafee2207019@stud.kuet.ac.bd>
2026-07-14 22:32:03 +0600

    NOTE-75 Fire NewReviewNotification when review is submitted

---
+++ app/Notifications/NewReviewNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Note;
+use App\Models\Review;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class NewReviewNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(
+        public Note $note,
+        public Review $review,
+        public User $reviewer,
+    ) {}
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'note_id' => $this->note->id,
+            'note_title' => $this->note->title,
+            'rating' => $this->review->rating,
+            'reviewer_name' => $this->reviewer->name,
+        ];
+    }
+}
